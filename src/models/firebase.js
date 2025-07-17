494535878f90da5d748b263febbe562f1a36690a
lcasserly <luciacasserly@gmail.com>
2025-07-17 11:00:53 -0300

    errores

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // Firebase configuration
 
 const firebaseConfig = {
-  //apiKey: "AIzaSyAeQ867-s9G5TagbT7N3-m_uSUzBIPXve4",
-  authDomain: "api-rest-casserlylucia.firebaseapp.com",
-  projectId: "api-rest-casserlylucia",
-  storageBucket: "api-rest-casserlylucia.firebasestorage.app",
-  messagingSenderId: "342256406195",
-  appId: "1:342256406195:web:20ec563a579c836f3c6ac6",
-  measurementId: "G-9YHL0GBV7D"
-};
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
